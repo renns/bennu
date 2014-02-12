9ea4390843daf78fccb3e076e4f32a12458d2a43
Glen Marchesani <glen@model3.net>
2014-02-12 14:26:06 -0500

    fix for json_xxx methods to work with clob's

--- bennu-extra-ddl.sql
+++ bennu-extra-ddl.sql
@@ -1,14 +1,38 @@
 
-
-CREATE FUNCTION json_str(json CLOB, path varchar(250)) RETURNS varchar(32767)
+CREATE FUNCTION jsonv_str(json LONGVARCHAR, path varchar(32767)) RETURNS LONGVARCHAR
    LANGUAGE JAVA DETERMINISTIC NO SQL
    EXTERNAL NAME 'CLASSPATH:com.qoid.bennu.util.HsqldbAssist.json_str'
 
+;;;
+
+CREATE FUNCTION jsonv_bool(json LONGVARCHAR, path varchar(32767)) RETURNS BOOLEAN
+   LANGUAGE JAVA DETERMINISTIC NO SQL
+   EXTERNAL NAME 'CLASSPATH:com.qoid.bennu.util.HsqldbAssist.json_bool'
+
+;;;
+
+CREATE FUNCTION jsonv_int(json LONGVARCHAR, path varchar(32767)) RETURNS INTEGER
+   LANGUAGE JAVA DETERMINISTIC NO SQL
+   EXTERNAL NAME 'CLASSPATH:com.qoid.bennu.util.HsqldbAssist.json_int'
+
+;;;
+
+CREATE FUNCTION json_str (json CLOB, path varchar(32767)) RETURNS LONGVARCHAR
+   RETURN jsonv_str(cast(json as LONGVARCHAR), path)
+
+;;;
+
+CREATE FUNCTION json_bool (json CLOB, path varchar(32767)) RETURNS LONGVARCHAR
+   RETURN jsonv_bool(cast(json as LONGVARCHAR), path)
+
+;;;
+
+CREATE FUNCTION json_int (json CLOB, path varchar(32767)) RETURNS LONGVARCHAR
+   RETURN jsonv_int(cast(json as LONGVARCHAR), path)
 
 ;;;
 
 
-CREATE FUNCTION json_str(json varchar(32767), path varchar(250)) RETURNS varchar(32767)
-   LANGUAGE JAVA DETERMINISTIC NO SQL
-   EXTERNAL NAME 'CLASSPATH:com.qoid.bennu.util.HsqldbAssist.json_str'
+
+
 
--- src/main/scala/com/qoid/bennu/util/HsqldbAssist.scala
+++ src/main/scala/com/qoid/bennu/util/HsqldbAssist.scala
@@ -15,4 +15,26 @@ object HsqldbAssist {
     case e: Exception => null
   }
 
+  def json_int(json: String, path: String)(): java.lang.Integer = try {
+    val jv = parseJson(json)
+    val pathPars = path.split("\\.")
+    pathPars.foldLeft(jv) { case (jv, v) => jv \ v } match {
+      case JInt(i) => i.toInt
+      case _ => null
+    }
+  } catch {
+    case e: Exception => null
+  }
+
+  def json_bool(json: String, path: String)(): java.lang.Boolean = try {
+    val jv = parseJson(json)
+    val pathPars = path.split("\\.")
+    pathPars.foldLeft(jv) { case (jv, v) => jv \ v } match {
+      case JBool(b) => b
+      case _ => null
+    }
+  } catch {
+    case e: Exception => null
+  }
+
 }
